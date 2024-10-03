e0216ba801a77cebf6ff65d99ab6283e29390841
alang602 <39152866+alang602@users.noreply.github.com>
2024-10-03 18:15:52 +0800

    Update 02-Hanoi.cpp

--- 扩展题目/递归/02-Hanoi.cpp
+++ 扩展题目/递归/02-Hanoi.cpp
@@ -1,1 +1,31 @@
 
+/*
+     汉诺塔（递归）
+*/
+#include <iostream>
+using namespace std;
+
+// 移动N个圆盘，从from到to，借助temp
+void MoveHanoi(int N, char from, char temp, char to) {
+    if (N == 1) {
+        cout << "move " << N << ":" << from << "--->" << to << endl;
+        return;
+    }
+    else {
+
+        MoveHanoi(N - 1, from, to, temp);
+        cout << "move " << N << ":" << from << "--->" << to << endl;
+        MoveHanoi(N - 1, temp, from, to);
+    }
+}
+
+
+int main()
+{
+    cout << "please input n:";
+    int N;
+    cin >> N;
+
+    MoveHanoi(N, 'A', 'B', 'C');
+
+}
