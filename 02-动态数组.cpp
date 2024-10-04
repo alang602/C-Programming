5df73134f3dcf9a34abd1559d90e1fb7ab365001
alang602 <39152866+alang602@users.noreply.github.com>
2024-10-04 10:15:22 +0800

    Update 02-动态数组.cpp

--- 02-动态数组.cpp
+++ 02-动态数组.cpp
@@ -3,7 +3,9 @@
 /*
      动态数组示例
 
-     原因：有时候题目是要求的数组的长度是根据输入N决定的，比如用户输入整数N，程序创建一个长度为N的数组。 这样动态创建数组的时候，需要会使用下面代码。
+     功能：有时候题目是要求的数组的长度是根据输入N决定的,
+          比如用户输入整数N，程序创建一个长度为N的数组。 
+          这样动态创建数组的时候，需要会使用下面代码。
      输入：整数n
      处理：创建一维数组array[n]，并给每个元素赋值
      输出：打印数组内容
@@ -21,7 +23,8 @@ int main()
     int n;
     cin >> n;
 
-    // Create Dynamic Array，pArray指向数组头，数组长度是n，每个元素大小是sizeof(int)
+    // Create Dynamic Array
+    // pArray指向数组头，数组长度是n，每个元素大小是sizeof(int)
     int* pArray = (int*)malloc(n* sizeof(int));
     if (pArray == NULL) {
         cout << "malloc failed";
@@ -69,6 +72,7 @@ int main()
     cin >> n;
 
     // Create Dynamic Array
+    // 使用new
     int* p = new int[n];
     for (int i = 0; i < n; i++) {
         p[i] = i;
@@ -96,6 +100,8 @@ int main()
 /*
       动态创建二维数组(m*n)
 
+      功能：根据用户输入两个整数m,n，程序创建一个长度为M*N的二维数组。
+            可以用在矩阵等问题
       输入： 两个整数(m ,n )
       输出： 二维数组, m行n列
 
@@ -115,7 +121,9 @@ int main()
     cin >> m;
     cin >> n;
 
-    //Create m*n 数组（m行，n列）
+    // Create m*n 数组（m行，n列）
+    // 先创建二维数组的头指针arr, 指向二维数组int**
+    // 再创建每行的一维数组的指针arr[i], 指向一维数组int*
     int** arr = (int**)malloc(m * sizeof(int*));
     for (int i = 0; i < m; i++) {
         arr[i] = (int*)malloc(n * sizeof(int));
