205ef6b409913fbbe898bac6938bf1c1c20e7df3
alang602 <39152866+alang602@users.noreply.github.com>
2024-10-06 13:22:05 +0800

    Create 08-gets.cpp

---
+++ 08-gets.cpp
@@ -0,0 +1,13 @@
+/* gets_s 用于从cin读取一行字符串 */
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    char str[20];
+    cout << "input a string:";
+    gets_s(str);
+    cout << "the string is:" << str;
+
+    char str
+}
